deb74095e0b93040bb2cac478694d7c03a552fd8
BlankHtmlPage <blankhtmlpager@gmail.com>
2024-07-04 15:18:59 +0300

    Add files via upload

---
+++ updater.py
@@ -0,0 +1,13 @@
+from time import sleep as w
+from os import system as sys
+
+w(2)
+with open('./updOrangeOS_Beta.exe', 'rb') as new_file:
+    with open('./OrangeOS_Beta.exe', 'wb') as old_file:
+        old_file.write(new_file.read())
+        print('Updated. Restarting...')
+        sys('del updOrangeOS_Beta.exe')
+        sys('cls||clear')
+        sys('./OrangeOS_Beta.exe')
+        exit(666)
+        
